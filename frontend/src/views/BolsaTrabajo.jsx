597c2b205df48e4825a09ef0bb48947c6c433b41
Gabriel Díaz Arevalillo <114990592+gabadiz@users.noreply.github.com>
2025-10-30 21:16:14 +0100

    bolsa

--- frontend/src/views/BolsaTrabajo.jsx
+++ frontend/src/views/BolsaTrabajo.jsx
@@ -4,20 +4,35 @@ import './BolsaTrabajo.css'
 
 
 export default function BolsaTrabajo() {
+
+  const funcbtn = () => {
+      const mensaje = document.getElementById("mensaje");
+      mensaje.classList.remove("oculto");
+      mensaje.classList.add("visible");
+      setTimeout(() => {
+          mensaje.classList.remove("visible");
+          mensaje.classList.add("oculto");
+      }, 2000);
+  }
+
   return(
         <div className="bolsaTrabajo-page">
           
           <SideMenu />
     
           <div className="bolsaTrabajo-blank">
-              <h1>Bolsa de Trabajo</h1>
+              <h1>Bolsa de Trabajo</h1><p> Todos los comentarios de texto a continuación son de ejemplo, ya que no tenemos una base de datos real todavía</p>
               <h2>Bienvenido a la bolsa de trabajo, donde podrás ser esclavizado voluntariamente!!! :DDDD</h2>
-              <button>Publicar Trabajo</button>
+              <button id="btn" onclick={funcbtn} >Crear bolsa</button>
+              <p className='oculto' id="mensaje">La bolsa fue creada con exito</p>
+
+
 
               <div className="marcoDeTrabajo">
                 <h2>Trabajo 1: </h2><h2>algoRaro</h2>
                 <div className="linea">
                   <p>Descripción del trabajo 1:</p><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <button>aplicar</button>
                 </div>
               </div>
 
@@ -25,8 +40,10 @@ export default function BolsaTrabajo() {
                 <h2>Trabajo 2: </h2><h2>algoMásRaro</h2>
                 <div className="linea">
                   <p>Descripción del trabajo 2: </p><p> Probando, todavía no hay datos disponibles ya que esto solo es un frontend.</p>
+                  <button>aplicar</button>
                 </div>
               </div>
+              
 
           </div>
           
